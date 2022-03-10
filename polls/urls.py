628091a3de92006eee8ba2b7015ab6568505949e
Juan David Díaz <juandiazr95@gmail.com>
2022-03-10 18:07:10 -0500

    Creando nuestras views y urls junto al template de la vista index

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+
 from . import views #del paquete polls import ...
 
 urlpatterns = [
-    path('', views.index, name='index') #index es la función que creé en views, le agrego un nombre descriptivo
+    #ex: /polls
+    path('', views.index, name='index'), #index es la función que creé en views, le agrego un nombre descriptivo
+    #ex: /polls/5
+    path('<question_id>', views.detail, name='detail'),
+    #ex: /polls/results
+    path('<question_id>/results', views.results, name='results'),
+    #ex: /polls/vote
+    path('<question_id>/vote', views.vote, name='vote')
 ]
