4f7cc541dd07def48241601686d72429cbf0df91
Juan David Díaz <juandiazr95@gmail.com>
2022-03-11 09:53:05 -0500

    Modularizando para evitar Hard Coding

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views #del paquete polls import ...
 
+app_name = 'polls'
 urlpatterns = [
     #ex: /polls
     path('', views.index, name='index'), #index es la función que creé en views, le agrego un nombre descriptivo
