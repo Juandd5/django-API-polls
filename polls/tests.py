da58f10b3b762c62d1060a1c32ab32b74b66a47d
Juan David Díaz <juandiazr95@gmail.com>
2022-03-12 16:48:22 -0500

    Primer test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionTest(TestCase):
+
+    def setUp(self):
+        self.question = Question(question_text = "¿Quién es el mejor Course Director de platzi?")
+
+    def test_was_created_recently_with_future_questions(self):
+        time = timezone.now() + timedelta(days=30)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_created_recently(), False)
+    
+    def test_was_created_recently_with_present_questions(self):
+        time = timezone.now() - timedelta(hours=23)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_created_recently(), True)
+    
+    def test_was_created_recently_with_past_questions(self):
+        time = timezone.now() - timedelta(days=1, minutes=1)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_created_recently(), False)
