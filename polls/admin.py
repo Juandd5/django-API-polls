8ccc8d48b9521fd11cdf4acca992e8829b928533
Juan David Díaz <juandiazr95@gmail.com>
2022-03-14 17:51:03 -0500

    Modificando el administrador de Django

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)#Esta función lleva como parámetro el modelo que queremos poner disponible en el adminitrador
-admin.site.register(Choice)
+
+#Con esto, cuando cree preguntas, puedo agregar hasta 3 respuestas.
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text'] #Organizo el orden en que se muestran estos campos
+    inlines = [ChoiceInLine] #Relaciono el ChoiceInLine
+    list_display = ('question_text', 'pub_date', 'was_created_recently') #Añado más campos para visualizar
+    list_filter = ['pub_date'] #añade un recuadro para filtrar por fechas
+    search_fields = ['question_text'] #añade un cuadro para buscar preguntas
+
+
+admin.site.register(Question, QuestionAdmin)#Esta función lleva como parámetro el modelo que queremos poner disponible en el adminitrador
+#admin.site.register(Choice)
